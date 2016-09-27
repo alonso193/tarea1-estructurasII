912621e09b2584c783b9375b29b78166fdc31fae
alonso193 <alonso.espinoza193@gmail.com>
2016-09-27 15:48:54 -0600

    main del programa
    de momento solo está obtniendo los parámetros que se le envían al ejecutable y guadandolos en una variable
    
    hace falta ligarlo con la clase cache para enviar estos parámetros

---
+++ main.cpp
@@ -0,0 +1,9 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char const *argv[]) {
+    int asociatividad = atoi(argv[1]);
+    int tamano_cache = atoi(argv[2]);
+    int tamano_bloque = atoi(argv[3]);
+    return 0;
+}
